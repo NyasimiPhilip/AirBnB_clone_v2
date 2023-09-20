a669dc4454006f0577f26bd49d43f09abdbe2392
nyasimiphilip@gmail.com <nyasimiphilip@gmail.com>
2023-09-20 08:51:48 +0300

    Updated the console.py

--- console.py
+++ console.py
@@ -73,7 +73,7 @@ class HBNBCommand(cmd.Cmd):
                 pline = pline[2].strip()  # pline is now str
                 if pline:
                     # check for *args or **kwargs
-                    if pline[0] is '{' and pline[-1] is'}'\
+                    if pline[0] == '{' and pline[-1] == '}'\
                             and type(eval(pline)) is dict:
                         _args = pline
                     else:
@@ -113,18 +113,48 @@ class HBNBCommand(cmd.Cmd):
         """ Overrides the emptyline method of CMD """
         pass
 
-    def do_create(self, args):
-        """ Create an object of any class"""
-        if not args:
+    def do_create(self, line):
+        """
+        Usage: create <class> <key 1>=<value 2> <key 2>=<value 2> ...
+        Create a new class instance with the given keys and values and print its id.
+        """
+        try:
+            if not line:
+                raise SyntaxError("Class name is missing")
+            # Split the input line into a list of words
+            my_list = line.split(" ")
+            # Create a dictionary to store keyword arguments
+            kwargs = {}
+            # Iterate through the words in the list starting from index 1
+            for i in range(1, len(my_list)):
+                key, value = tuple(my_list[i].split("="))
+                # Check if the value is enclosed in double quotes and clean it
+                if value[0] == '"':
+                    value = value.strip('"').replace("_", " ")
+                else:
+                    try:
+                        # Try to evaluate the value as Python expression
+                        value = eval(value)
+                    except (SyntaxError, NameError):
+                        # Ignore errors and continue to the next key-value pair
+                        continue
+                # Store the key-value pair in the kwargs dictionary
+                kwargs[key] = value
+            if kwargs == {}:
+                # Create an object of the specified class without arguments
+                obj = eval(my_list[0])()
+            else:
+                # Create an object of the specified class with the provided kwargs
+                obj = eval(my_list[0])(**kwargs)
+                storage.new(obj)
+            # Print the id of the created object
+            print(obj.id)
+            # Save the object to the storage system
+            obj.save()
+        except SyntaxError:
             print("** class name missing **")
-            return
-        elif args not in HBNBCommand.classes:
+        except NameError:
             print("** class doesn't exist **")
-            return
-        new_instance = HBNBCommand.classes[args]()
-        storage.save()
-        print(new_instance.id)
-        storage.save()
 
     def help_create(self):
         """ Help information for the create method """
@@ -272,7 +302,7 @@ class HBNBCommand(cmd.Cmd):
                 args.append(v)
         else:  # isolate args
             args = args[2]
-            if args and args[0] is '\"':  # check for quoted arg
+            if args and args[0] == '\"':  # check for quoted arg
                 second_quote = args.find('\"', 1)
                 att_name = args[1:second_quote]
                 args = args[second_quote + 1:]
@@ -280,10 +310,10 @@ class HBNBCommand(cmd.Cmd):
             args = args.partition(' ')
 
             # if att_name was not quoted arg
-            if not att_name and args[0] is not ' ':
+            if not att_name and args[0] != ' ':
                 att_name = args[0]
             # check for quoted val arg
-            if args[2] and args[2][0] is '\"':
+            if args[2] and args[2][0] == '\"':
                 att_val = args[2][1:args[2].find('\"', 1)]
 
             # if att_val was not quoted arg
