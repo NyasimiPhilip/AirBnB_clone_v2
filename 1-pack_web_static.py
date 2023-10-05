0898ea5631a7377065e26b4bc4e3a09f2233d472
nyasimiphilip@gmail.com <nyasimiphilip@gmail.com>
2023-10-05 09:25:34 +0300

    Update

---
+++ 1-pack_web_static.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+from datetime import datetime
+from fabric.api import local
+from time import strftime
+
+
+def do_pack():
+    """script that generates .tgz archive from the
+    contents of the web_static folder"""
+    
+    filedate = strftime("%Y%m%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        local("tar -czvf versions/web_static_{}.tgz web_static/"
+              .format(filedate))
+        # local(f"python -m tarfile -vc versions/{filedate} web_static/")
+        return "versions/web_static_{}.tgz".format(filedate)
+    
+    except Exception as e:
+        print(e)
+        return None
